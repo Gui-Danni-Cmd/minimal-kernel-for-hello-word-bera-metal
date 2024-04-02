1b2559889c21fb771f14ab8bc0a2d07569d06f7e
Guilherme Dannilo Maciel Coelho <gmacielcoelho@gmail.com>
2024-04-01 23:02:34 -0300

    final inst

--- src/kernel.c
+++ src/kernel.c
@@ -1,31 +1,30 @@
 #include "./kernel.h"
 
-uint16 vga_entry(unsigned char ch, uint8 fore_color, uint8 back_color) {
-    return ((uint16)back_color << 12) | ((uint16)fore_color << 8) | ch;
+uint16 vga_entry(uint8 ch, uint8 fundo_color, uint8 text_color) {
+    return ((uint16)fundo_color << 12) | ((uint16)text_color << 8) | ch;
 }
 
-void clear_vga_buffer(uint16 **buffer, uint8 tex_color, uint8 fundo_color)
+void clear_vga_buffer(uint16 **buffer, uint8 fundo_color, uint8 tex_color)
 {
-    uint32 i;
-  for( i = 0; i < BUFSIZE; i++){
-    (*buffer)[i] = vga_entry(NULL, tex_color, fundo_color);
+  for( uint16 i = 0; i < BUFSIZE; i++){
+    (*buffer)[i] = vga_entry(NULL, fundo_color, tex_color);
   }
 }
 
-void init_vga(uint8 tex_color, uint8 fundo_color) {
+void init_vga(uint8 fundo_color, uint8 tex_color) {
   vga_buff = (uint16*)VGA_Adrr;  
-  clear_vga_buffer(&vga_buff, tex_color, fundo_color);  
+  clear_vga_buffer(&vga_buff, fundo_color, tex_color);
 }
 
 void Main(void)
 {
-    const char *string = "Ola mundo!\0";
-    init_vga(WHITE, BLUE);
+    const char *string = "System init";
+    init_vga(BLACK, BRIGHT_CYAN);
    
-    uint16 cont = 0;
+    uint8 cont = 0;
 
     while( string[cont] != '\0' ) {
-        vga_buff[cont] = vga_entry((char)string[cont], GREEN, BLUE);
+        vga_buff[cont] = vga_entry((char)string[cont], BLACK, BRIGHT_CYAN);
         cont++;
     }
 
